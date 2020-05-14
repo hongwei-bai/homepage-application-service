85f3f045bb4d1ebf9ae7c842e374d1752eb4a8bf
Hongwei Bai <bhw8412@hotmail.com>
2020-05-14 22:51:59 +1000

    add covid19 api

---
+++ src/main/java/com/hongwei/model/arcgis/covid19/Features.java
@@ -0,0 +1,5 @@
+package com.hongwei.model.arcgis.covid19;
+
+public class Features {
+    public Attributes attributes;
+}
---
+++ src/main/java/com/hongwei/model/covid19/AusDataByState.java
@@ -0,0 +1,5 @@
+package com.hongwei.model.covid19;
+
+public class AusDataByState {
+    public AusDataByStatePerDay[] ausDataByStatePerDays;
+}
