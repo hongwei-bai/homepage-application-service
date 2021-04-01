f35b4a225f6c29b8085a676d254ec98f43928301
Hongwei <bhw8412@hotmail.com>
2021-04-01 23:49:12 +1100

    officially fix cors pre-flight issue, by documented references

--- src/main/java/com/hongwei/security/filters/JwtRequestFilter.kt
+++ src/main/java/com/hongwei/security/filters/JwtRequestFilter.kt
@@ -3,7 +3,10 @@ package com.hongwei.security.filters
 import com.hongwei.constants.SecurityConfigurations
 import com.hongwei.security.service.AuthorisationService
 import com.hongwei.security.service.MyUserDetailsService
+import org.apache.log4j.LogManager
+import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
@@ -17,6 +20,8 @@ import javax.servlet.http.HttpServletResponse
 
 @Component
 class JwtRequestFilter : OncePerRequestFilter() {
+    private val _logger: Logger = LogManager.getLogger(JwtRequestFilter::class.java)
+
     @Autowired
     private lateinit var userDetailsService: MyUserDetailsService
 
@@ -30,7 +35,12 @@ class JwtRequestFilter : OncePerRequestFilter() {
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
         val authorizationHeader = request.getHeader(securityConfigurations.authorizationHeader)
 
-        if (doCORSFilter(request, response)) {
+        /*
+        The W3 spec for CORS preflight requests clearly states that user credentials should be excluded.
+        Reference[a]: https://stackoverflow.com/questions/15734031/why-does-the-preflight-options-request-of-an-authenticated-cors-request-work-in
+        Reference[b]: https://fetch.spec.whatwg.org/#cors-protocol-and-credentials
+         */
+        if (request.method == HttpMethod.OPTIONS.name) {
             grantAccess(request)
         } else if (authorizationHeader != null && authorizationHeader.startsWith(securityConfigurations.authorizationBearer)) {
             val jwt = authorizationHeader.substring(securityConfigurations.authorizationBearer.length + 1)
@@ -43,22 +53,6 @@ class JwtRequestFilter : OncePerRequestFilter() {
         }
 
         chain.doFilter(request, response)
-    }
-
-    private fun doCORSFilter(request: HttpServletRequest, response: HttpServletResponse): Boolean {
-        // Authorize (allow) all domains to consume the content
-//        response.addHeader("Access-Control-Allow-Origin", "*")
-//        response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST")
-//        response.addHeader("Access-Control-Request-Headers", securityConfigurations.authorizationHeader)
-
-        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
-        if (request.method == "OPTIONS") {
-//            response.status = HttpServletResponse.SC_ACCEPTED
-            return true
-        }
-
-        // pass the request along the filter chain
-        return false
     }
 
     private fun grantAccess(request: HttpServletRequest) {
