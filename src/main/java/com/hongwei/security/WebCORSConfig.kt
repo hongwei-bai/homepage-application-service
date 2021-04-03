4ccea7a3961f773538c4f57f29d2f355e854ce5b
Hongwei <bhw8412@hotmail.com>
2021-04-03 22:43:49 +1100

    enable PUT method for CORS

--- src/main/java/com/hongwei/security/WebCORSConfig.kt
+++ src/main/java/com/hongwei/security/WebCORSConfig.kt
@@ -3,6 +3,7 @@ package com.hongwei.security
 import com.hongwei.constants.SecurityConfigurations
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -15,6 +16,12 @@ open class WebCORSConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+                .allowedMethods(
+                        HttpMethod.GET.name,
+                        HttpMethod.POST.name,
+                        HttpMethod.OPTIONS.name,
+                        HttpMethod.PUT.name
+                )
                 .allowedOrigins(*securityConfigurations.corsAllowDomains.toTypedArray())
     }
 }
